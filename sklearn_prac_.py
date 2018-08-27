1ad60c05ccf8668fe0f16219049e0cf3b0dc54d3
jakinpilla <jakinpilla@gmail.com>
2018-08-27 13:44:00 +0900

    modified

---
+++ sklearn_prac_.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 13:31:04 2018
+
+@author: Daniel
+"""
+
+from os import getcwd, chdir
+getcwd()
+chdir('C:/Users/Daniel/scikit_learn')
+
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
